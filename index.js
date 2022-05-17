b2b312a339779d7874c57cde22bb58b38caef0b3
immyz <imran.uddin@mail.utoronto.ca>
2022-05-17 13:31:55 -0400

    added logic to create a randomized the game pattern

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$(document).on("click", function () {
-    alert("I Work");
-});
+let buttonCol = {1: "red", 2: "blue", 3: "green", 4: "yellow"}
+let gamePattern = []
+let round = 3
+
+function nextSequence() {
+    let randNum = Math.floor(Math.random()*3);
+    return randNum
+}
+
+for (let i = 0; i < round; i++) {
+    gamePattern.push(buttonCol.nextSequence)
+}
+
